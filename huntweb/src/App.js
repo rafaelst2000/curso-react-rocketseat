0c21856118cfb40833843d82fc069a304b281524
rafaelst2000 <rafaelst2000@gmail.com>
2020-05-15 13:49:42 -0300

    front end

---
+++ huntweb/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './components/Header'
+import Main from './pages/main'
+
+import Routes from './routes'
+
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+)
+
+export default App;
